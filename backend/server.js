ed68e3edf0da24008425d553afcb7921aa9cb321
leon-ronaldo <leonronaldo.trs@gmail.com>
2026-01-28 15:33:29 +0530

    update: cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 
